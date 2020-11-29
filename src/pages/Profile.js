c3b97672ff3f415627a87ae4480a700d5073b0e2
HeldensBrecht <brecht.heldens@student.ehb.be>
2020-11-29 12:38:25 +0100

    Oplossen eslint warning voor useEffect dependencies

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,19 +2,24 @@ import React, { useState, useEffect } from "react";
 
 export default function Profile(props) {
   const [profile, setProfile] = useState(null);
+  // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState("");
 
   useEffect(() => {
-    loadUserProfile();
-  }, [props]);
-
-  const loadUserProfile = () => {
     console.log(props);
     props.auth.getProfile((profile, err) => {
       setProfile(profile);
       setError(err);
     });
-  };
+  }, [props]);
+
+  // const loadUserProfile = () => {
+  //   console.log(props);
+  //   props.auth.getProfile((profile, err) => {
+  //     setProfile(profile);
+  //     setError(err);
+  //   });
+  // };
 
   return (
     <>
