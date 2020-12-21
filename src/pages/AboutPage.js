c0619952a8381a5ae696db4086acdcb873dca543
HeldensBrecht <brecht.heldens@student.ehb.be>
2020-12-21 14:58:24 +0100

    Push for env update on aws

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,6 +5,7 @@ const AboutPage = () => {
     <main className="container-fluid">
       <div className="my-md-5" style={{ height: "5rem" }}></div>
       <h2>About Us</h2>
+      {/*  */}
     </main>
   );
 };
