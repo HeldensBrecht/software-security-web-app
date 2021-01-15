66f483e862b841cad54d41ae39da08abfa8f514f
HeldensBrecht <brecht.heldens@student.ehb.be>
2021-01-15 12:48:54 +0100

    Filled up AboutPage with dummy text

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,7 +5,78 @@ const AboutPage = () => {
     <main className="container-fluid">
       <div className="my-md-5" style={{ height: "5rem" }}></div>
       <h2>About Us</h2>
-      {/*  */}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Mi ipsum faucibus
+        vitae aliquet nec ullamcorper sit. Dictum non consectetur a erat nam at.
+        Velit egestas dui id ornare arcu odio. Fermentum leo vel orci porta non
+        pulvinar. Quis vel eros donec ac. Elementum facilisis leo vel fringilla
+        est ullamcorper. Felis eget nunc lobortis mattis aliquam faucibus purus.
+        Vel quam elementum pulvinar etiam non. Odio eu feugiat pretium nibh
+        ipsum consequat. Nulla posuere sollicitudin aliquam ultrices sagittis.
+        Enim sed faucibus turpis in eu mi bibendum neque egestas. Eget nullam
+        non nisi est. Integer quis auctor elit sed vulputate mi. Nibh cras
+        pulvinar mattis nunc. Viverra tellus in hac habitasse. Quis commodo odio
+        aenean sed adipiscing diam donec adipiscing tristique. Nunc sed velit
+        dignissim sodales ut eu.
+      </p>
+      <p>
+        Ut tellus elementum sagittis vitae et. Nisl suscipit adipiscing bibendum
+        est ultricies. Nullam vehicula ipsum a arcu cursus. Sit amet nisl
+        suscipit adipiscing bibendum est ultricies. Risus at ultrices mi tempus
+        imperdiet nulla malesuada pellentesque elit. Integer quis auctor elit
+        sed vulputate mi. Mauris sit amet massa vitae tortor condimentum lacinia
+        quis. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Et
+        sollicitudin ac orci phasellus. Nullam non nisi est sit amet facilisis.
+        Est ante in nibh mauris cursus mattis molestie a iaculis. Quis imperdiet
+        massa tincidunt nunc pulvinar. Dictumst vestibulum rhoncus est
+        pellentesque elit ullamcorper dignissim cras tincidunt. Nisi lacus sed
+        viverra tellus. Eget est lorem ipsum dolor.
+      </p>
+      <p>
+        Ac placerat vestibulum lectus mauris ultrices eros in cursus. Erat velit
+        scelerisque in dictum non consectetur a erat. Lacus vel facilisis
+        volutpat est velit. Tellus pellentesque eu tincidunt tortor aliquam
+        nulla. Id ornare arcu odio ut sem nulla pharetra. Tincidunt arcu non
+        sodales neque. Nunc aliquet bibendum enim facilisis gravida neque
+        convallis a cras. Amet volutpat consequat mauris nunc congue nisi.
+        Imperdiet proin fermentum leo vel orci porta non pulvinar neque. Purus
+        in massa tempor nec. Egestas sed sed risus pretium quam. Donec enim diam
+        vulputate ut. Nec feugiat nisl pretium fusce id velit. Neque aliquam
+        vestibulum morbi blandit cursus risus.
+      </p>
+      <p>
+        Nullam non nisi est sit amet. Pretium lectus quam id leo in vitae
+        turpis. Sapien eget mi proin sed libero enim sed. Risus at ultrices mi
+        tempus imperdiet nulla malesuada pellentesque elit. Mattis aliquam
+        faucibus purus in massa tempor nec feugiat nisl. Auctor eu augue ut
+        lectus arcu. Mi in nulla posuere sollicitudin aliquam ultrices sagittis
+        orci. Adipiscing diam donec adipiscing tristique risus nec feugiat. Ac
+        turpis egestas sed tempus. Egestas egestas fringilla phasellus faucibus
+        scelerisque eleifend donec pretium. Vitae turpis massa sed elementum
+        tempus egestas. Bibendum at varius vel pharetra vel turpis nunc. Sem
+        integer vitae justo eget magna. Nibh mauris cursus mattis molestie a
+        iaculis. Viverra adipiscing at in tellus integer feugiat scelerisque
+        varius. Luctus venenatis lectus magna fringilla urna porttitor.
+        Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Nisl
+        rhoncus mattis rhoncus urna.
+      </p>
+      <p>
+        Molestie a iaculis at erat pellentesque adipiscing commodo elit. Quis
+        auctor elit sed vulputate mi sit amet mauris. Est ante in nibh mauris
+        cursus mattis molestie a iaculis. Non sodales neque sodales ut etiam.
+        Diam quis enim lobortis scelerisque. Est ultricies integer quis auctor
+        elit sed vulputate mi sit. Pellentesque habitant morbi tristique
+        senectus et netus et. Pellentesque sit amet porttitor eget dolor morbi
+        non arcu risus. Mollis aliquam ut porttitor leo a. Praesent elementum
+        facilisis leo vel fringilla est ullamcorper eget nulla. Condimentum
+        mattis pellentesque id nibh. Risus commodo viverra maecenas accumsan.
+        Aliquam malesuada bibendum arcu vitae elementum. Ultrices mi tempus
+        imperdiet nulla malesuada pellentesque elit eget. Adipiscing diam donec
+        adipiscing tristique risus nec feugiat. Volutpat maecenas volutpat
+        blandit aliquam etiam erat. Consectetur libero id faucibus nisl
+        tincidunt. A lacus vestibulum sed arcu non.
+      </p>
     </main>
   );
 };
--- src/pages/Callback.js
+++ src/pages/Callback.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { toast } from "react-toastify";
 
@@ -9,7 +9,6 @@ const Callback = (props) => {
 
   useEffect(() => {
     if (!isLoading) {
-      console.log(user);
       getAccessTokenSilently()
         .then((accessToken) => {
           userApi
